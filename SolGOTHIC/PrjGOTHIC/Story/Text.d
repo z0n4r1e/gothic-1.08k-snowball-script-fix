361641a5d46f2701eb31d7102a8c0deb356283c6
z0n4r1e <135639955+z0n4r1e@users.noreply.github.com>
2023-12-22 17:01:05 +0200

    Add files via upload

--- SolGOTHIC/PrjGOTHIC/Story/Text.d
+++ SolGOTHIC/PrjGOTHIC/Story/Text.d
@@ -1,1 +1,379 @@
 
+const string MOBNAME_BACKPACK_1 = "Сумка";
+const string MOBNAME_CRATE = "Коробка";
+const string MOBNAME_CHEST = "Сундук";
+const string MOBNAME_BED = "Кровать";
+const string MOBNAME_DOOR = "Дверь";
+const string MOBNAME_CAMP = "Привал";
+const string MOBNAME_TORCH = "Факел";
+const string MOBNAME_TORCHHOLDER = "Держатель";
+const string MOBNAME_BARBQ_SCAV = "Вертел";
+const string MOBNAME_BARRELO_OC = "Бочка";
+const string MOBNAME_BENCH = "Скамья";
+const string MOBNAME_ANVIL = "Наковальня";
+const string MOBNAME_BUCKET = "Ведро";
+const string MOBNAME_FORGE = "Горнило";
+const string MOBNAME_GRINDSTONE = "Точило";
+const string MOBNAME_WHEEL = "Лебедка";
+const string MOBNAME_LAB = "Лаборатория";
+const string MOBNAME_BOOK = "Книга";
+const string MOBNAME_CHAIR = "Стул";
+const string MOBNAME_CAULDRON = "Котел";
+const string MOBNAME_THRONE = "Трон";
+const string MOBNAME_PAN = "Сковорода";
+const string MOBNAME_REPAIR = "Мастерская";
+const string MOBNAME_WATERPIPE = "Кальян";
+const string MOBNAME_LADDER = "Лестница";
+const string MOBNAME_SWITCH = "Рычаг";
+const string MOBNAME_BARONSTHRONE = "Престол";
+const string MOBNAME_ORE = "Руда";
+const string MOBNAME_ROPEWAY = "Мостовая";
+const string MOBNAME_STOMPER = "Дробилка";
+const string MOBNAME_BELLOW = "Мехи";
+const string MOBNAME_STONEMILL = "Жернова";
+const string MOBNAME_ORC_MUMMY = "Муммия";
+const string MOBNAME_PILLAR = "Колонна";
+const string MOBNAME_BASKET_RICE = "Корзина";
+const string MOBNAME_DRUM = "Барабан";
+const string MOBNAME_GROUND_SLOT = "Жолоб";
+const string MOBNAME_HERB_PSI = "Пресс";
+const string MOBNAME_IDOL = "Статуэтка";
+const string MOBNAME_LEAN_WALL = "Опора";
+const string MOBNAME_ORCDRUM = "Тамбурин";
+const string MOBNAME_TARGETSTONE = "Мишень";
+const string MOBNAME_SHRINE = "Алтарь";
+const string MOBNAME_SWORDSTONE = "Пьедестал";
+
+const string TXT_GUILDS[42] =
+{
+	"(нет)",
+	"Барон",
+	"Стражник",
+	"Призрак",
+	"Маг Огня",
+	"Рудокоп",
+	"Маг Воды",
+	"Наемник",
+	"Вор",
+	"Крестьянин",
+	"Скребок",
+	"Гуру",
+	"Послушник",
+	"Страж",
+	"Некромант",
+	"Девица",
+	"",
+	"Ящерица",
+	"Спящий",
+	"Гоблин",
+	"Тролль",
+	"Глорх",
+	"Ползун",
+	"Мясной жук",
+	"Падальщик",
+	"Демон",
+	"Волк",
+	"Мракорис",
+	"Шершень",
+	"Болотожор",
+	"Зомби",
+	"Орк-зомби",
+	"Скелетон",
+	"Орочья гончая",
+	"Кротокрыс",
+	"Голем",
+	"Шныг",
+	"",
+	"Шаман орков",
+	"Воин орков",
+	"Разведчик орков",
+	"Орк-раб"
+};
+
+const string TXT_SPELLS[59] =
+{
+	"Свет",
+	"Огненный шар",
+	"нет",
+	"Страх",
+	"Исцеление",
+	"нет",
+	"Создать демона",
+	"Создать скелетонов",
+	"нет",
+	"Кулак ветра",
+	"Телекинез",
+	"Чары",
+	"Сон",
+	"Пирокинез",
+	"Кольцо смерти",
+	"Контроль",
+	"Смерть нежити",
+	"Огненная стрела",
+	"Огненный шторм",
+	"Огненный дождь",
+	"нет",
+	"Портал к Магам Огня",
+	"Портал к Магам Воды",
+	"Портал к Некроманту",
+	"Портал к оркам",
+	"Портал в Болотный Лагерь",
+	"Трансформация в шершня",
+	"нет",
+	"Трансформация в ползуна",
+	"Трансформация в шныгу",
+	"Трансформация в мясного жука",
+	"Трансформация в кротокрыса",
+	"Трансформация в орочью гончую",
+	"нет",
+	"Трансформация в падальщика",
+	"n/a",
+	"Трансформация в мракориса",
+	"Трансформация в глорха",
+	"Трансформация в ящерицу",
+	"Трансформация в волка",
+	"Молния",
+	"Ледяная стрела",
+	"Шаровая молния",
+	"Ледяная глыба",
+	"Ледяная волна",
+	"Создать голема",
+	"Аримия тьмы",
+	"Кулак шторма",
+	"нет",
+	"Дыхание смерти",
+	"Исчезновение монстра",
+	"нет",
+	"нет",
+	"Берсеркер",
+	"Гнем Уризеля",
+	"Новый2",
+	"Новый3",
+	"Новый4",
+	"Новый5"
+};
+
+const string TXT_TALENTS[12] =
+{
+	"",
+	"Одноручный меч",
+	"Двуручный меч",
+	"Лук",
+	"Арбалет",
+	"Вскрытие замков",
+	"Кража",
+	"Магия",
+	"Подкрадывание",
+	"Восстановление",
+	"Мастер огня",
+	"Акробатика"
+};
+
+const string TXT_TALENTS_SKILLS[12] =
+{
+	"",
+	"не обучен|обучен|мастер",
+	"не обучен|обучен|мастер",
+	"не обучен|обучен|мастер",
+	"не обучен|обучен|мастер",
+	"не обучен|обучен|мастер",
+	"не обучен|обучен|мастер",
+	"не обучен|Круг 1|Круг 2|Круг 3|Круг 4|Круг 5|Круг 6",
+	"не обучен|обучен",
+	"|",
+	"|",
+	"не обучен|обучен"
+};
+
+const string TXT_INV_CAT[9] =
+{
+	"",
+	"Оружие",
+	"Доспехи",
+	"Магия",
+	"Артефакты",
+	"Еда",
+	"Эликсиры",
+	"Документы",
+	"Разное"
+};
+const string NAME_Buddler = "Рудокоп";
+const string NAME_Schatten = "Призрак";
+const string NAME_ConvoiTraeger = "Носильщик";
+const string NAME_ConvoiWache = "Охранник";
+const string NAME_Torwache = "Стражник у ворот";
+const string NAME_Brueckenwache = "Стражник у моста";
+const string NAME_Gardist = "Стражник";
+const string NAME_Bauer = "Крестьянин";
+const string NAME_Schlaeger = "Головорез";
+const string NAME_Schuerfer = "Скребок";
+const string NAME_Organisator = "Вор";
+const string NAME_Soeldner = "Наемник";
+const string NAME_Novize = "Послушник";
+const string NAME_Templer = "Страж";
+const string NAME_Raeuber = "Головорез";
+const string NAME_Tuersteher = "Громила";
+const string NAME_MadNovice = "Фанатичный послушник";
+const string NAME_MadTemplar = "Фанатичный страж";
+const string NAME_MadTemplar2 = "Апокалипсический страж";
+const string DIALOG_ENDE = "ЗАКОНЧИТЬ";
+const string DIALOG_BACK = "НАЗАД";
+const string DIALOG_TRADE = "(Торговать)";
+const string NAME_Ring = "Кольцо";
+const string NAME_Amulett = "Амулет";
+const string NAME_Trank = "Эликсир";
+const string NAME_Rune = "Руна";
+const string NAME_Spruchrolle = "Свиток";
+const string NAME_Value = "Ценность:";
+const string NAME_Mag_Circle = "Круг:";
+const string NAME_Manakosten = "Стоит маны:";
+const string NAME_ManakostenMax = "Стоит маны (макс.):";
+const string NAME_ManaPerSec = "Маны в сек.";
+const string NAME_Duration = "Длительность (мин.)";
+const string NAME_Mana_needed = "Требуется маны:";
+const string NAME_Str_needed = "Требуется силы:";
+const string NAME_Dex_needed = "Требуется ловкости:";
+const string NAME_Spell_Load = "Заклинание заряда";
+const string NAME_Spell_Invest = "Заклининие покрова";
+const string NAME_Dam_Edge = "Урон от оружия";
+const string NAME_Dam_Point = "Урон от стрелы";
+const string NAME_Dam_Fire = "Урон от огня";
+const string NAME_Dam_Magic = "Урон от магии";
+const string NAME_Dam_Fly = "Урон от электричества";
+const string NAME_Damage = "Урон:";
+const string NAME_PerMana = " (за единицу маны)";
+const string NAME_DamagePerSec = "Урон в секунду";
+const string NAME_Prot_Edge = "Защита от оружия:";
+const string NAME_Prot_Point = "Защита от стрел:";
+const string NAME_Prot_Fire = "Защита от огня:";
+const string NAME_Prot_Magic = "Защита от магии:";
+const string NAME_Bonus_HP = "Бонус к жизни:";
+const string NAME_Bonus_Mana = "Бонус к мане:";
+const string NAME_Bonus_HpMax = "Бонус за максимальную жизнь:";
+const string NAME_Bonus_ManaMax = "Бонус за максимальную ману:";
+const string NAME_Bonus_Dex = "Бонус к ловкости:";
+const string NAME_Bonus_Str = "Бонус к силе:";
+const string NAME_OneHanded = "Одноручное оружие";
+const string NAME_TwoHanded = "Двуручное оружие";
+const string NAME_HealingPerMana = "Исцеление за ед. маны";
+const string NAME_BuyAttributeSTR = "Сила +";
+const string NAME_BuyAttributeDEX = "Ловкость +";
+const string NAME_BuyAttributeMAN = "Мана +";
+const string NAME_XPGained = "опыт +";
+const string NAME_LevelUp = "Достигнут новый уровень!";
+const string NAME_NewLogEntry = "Новая запись в дневнике";
+const string _STR_INVALID = "(неизвестно)-";
+const string _STR_ATTRIBUTE_HITPOINTS = "жизни";
+const string _STR_ATTRIBUTE_HITPOINTS_MAX = "максимальной жизни";
+const string _STR_ATTRIBUTE_MANA = "маны";
+const string _STR_ATTRIBUTE_MANA_MAX = "максимальной маны";
+const string _STR_ATTRIBUTE_STRENGTH = "силы";
+const string _STR_ATTRIBUTE_DEXTERITY = "ловкости";
+const string _STR_ATTRIBUTE_MAGIC_CIRCLE = "Для использования этого предмета необходимо знание ";
+const string _STR_CANNOTUSE_PRE_PLAYER = "Для использования этого предмета вам не хватает ";
+const string _STR_CANNOTUSE_POINTS = "ед. ";
+const string _STR_CANNOTUSE_LEVELS = " Круга Магии!";
+const string _STR_CANNOTUSE_POST = "!";
+const string _STR_CANNOTUSE_PRE_NPC = " (";
+const string _STR_CANNOTUSE_POST_NPC = ")  не хватает";
+const string _STR_MESSAGE_INTERACT_NO_KEY = "Нет отмычки или ключа.";
+const string _STR_MESSAGE_PICKLOCK_SUCCESS = "звучит хорошо.";
+const string _STR_MESSAGE_PICKLOCK_UNLOCK = "Замок открыт.";
+const string _STR_MESSAGE_PICKLOCK_FAILURE = "Черт... все сначала.";
+const string _STR_MESSAGE_PICKLOCK_BROKEN = "Отмычка сломалась.";
+const string _STR_MESSAGE_OCLEVER_STUCKS = "Рычаг не двигается.";
+const string _STR_MESSAGE_OCLEVER_MOVES = "Статуэтка двигает рычаг.";
+const string _STR_MESSAGE_WHEEL_STUCKS = "Лебедка заела.";
+const string _STR_MESSAGE_ITEM_GIVEN = "Отдан 1 предмет";
+const string _STR_MESSAGE_ITEMS_GIVEN = "Отдано предметов: ";
+const string _STR_MESSAGE_ORE_GIVEN = "Отдано руды: ";
+const string _STR_MESSAGE_ITEM_TAKEN = "Получен 1 предмет";
+const string _STR_MESSAGE_ITEMS_TAKEN = "Получено предметов: ";
+const string _STR_MESSAGE_ORE_TAKEN = "Получено руды: ";
+const string _STR_MESSAGE_TRADE_FAILURE = "Стоимость товаров недостаточно высока.";
+const string _STR_MESSAGE_Joincamp = "Условие принятия: Уровень ";
+const string _STR_MESSAGE_CANNOTSTEAL = "Кража невозможна.";
+
+const int _YPOS_MESSAGE_GIVEN = 37;
+const int _YPOS_MESSAGE_TAKEN = 40;
+const int _YPOS_MESSAGE_LOGENTRY = 45;
+const int _YPOS_MESSAGE_XPGAINED = 50;
+const int _YPOS_MESSAGE_LEVELUP = 55;
+const int _YPOS_MESSAGE_Joincamp = 50;
+const int _YPOS_MESSAGE_PICKLOCK_SUCCESS = 6;
+const int _YPOS_MESSAGE_PICKLOCK_UNLOCK = 9;
+const int _YPOS_MESSAGE_PICKLOCK_FAILURE = 12;
+const int _YPOS_MESSAGE_PICKLOCK_BROKEN = 15;
+const int _YPOS_MESSAGE_CANNOTSTEAL = 6;
+const int _TIME_MESSAGE_GIVEN = 4;
+const int _TIME_MESSAGE_TAKEN = 4;
+const int _TIME_MESSAGE_LOGENTRY = 4;
+const int _TIME_MESSAGE_XPGAINED = 3;
+const int _TIME_MESSAGE_LEVELUP = 5;
+const int _TIME_MESSAGE_RAISEATTRIBUTE = 4;
+const int _TIME_MESSAGE_Joincamp = 5;
+const int _TIME_MESSAGE_PICKLOCK = 3;
+const int _TIME_MESSAGE_CANNOTSTEAL = 2;
+
+const string NAME_BuyArmorPrefix = " (";
+const string NAME_BuyArmorPostfix = " руды)";
+const string NAME_LearnPrefix = " (";
+const string NAME_LearnMidfix = " руды, ";
+const string NAME_LearnPostfixS = "оч. обучения)";
+const string NAME_LearnPostfixP = "оч. обучения)";
+const string NAME_LearnStrength_1 = "Сила +1";
+const string NAME_LearnStrength_5 = "Сила +5";
+const string NAME_LearnStrength_10 = "Сила +10";
+const string NAME_LearnStrength_20 = "Сила +20";
+const string NAME_LearnDexterity_1 = "Ловкость +1";
+const string NAME_LearnDexterity_5 = "Ловкость +5";
+const string NAME_LearnDexterity_10 = "Ловкость +10";
+const string NAME_LearnDexterity_20 = "Ловкость +20";
+const string NAME_LearnMana_1 = "Мана +1";
+const string NAME_LearnMana_5 = "Мана +5";
+const string NAME_LearnMana_10 = "Мана +10";
+const string NAME_LearnMana_20 = "Мана +20";
+const string NAME_Learn1h_1 = "Одноручный меч, уровень 1";
+const string NAME_Learn1h_2 = "Одноручный меч, уровень 2";
+const string NAME_Learn2h_1 = "Двуручный меч, уровень 1";
+const string NAME_Learn2h_2 = "Двуручный меч, уровень 1";
+const string NAME_LearnBow_1 = "Лук, уровень 1";
+const string NAME_LearnBow_2 = "Лук, уровень 2";
+const string NAME_LearnCrossbow_1 = "Арбалет, уровень 1";
+const string NAME_LearnCrossbow_2 = "Арбалет, уровень 2";
+const string NAME_LearnMage_1 = "Первый круг магии";
+const string NAME_LearnMage_2 = "Второй круг магии";
+const string NAME_LearnMage_3 = "Третий круг магии";
+const string NAME_LearnMage_4 = "Четвертый круг магии";
+const string NAME_LearnMage_5 = "Пятый круг магии";
+const string NAME_LearnMage_6 = "Шестой круг магии";
+const string NAME_LearnPickpocket_1 = "Кража, уровень 1";
+const string NAME_LearnPickpocket_2 = "Кража, уровень 2";
+const string NAME_LearnPicklock_1 = "Вскрывание замков, уровень 1";
+const string NAME_LearnPicklock_2 = "Вскрывание замков, уровень 2";
+const string NAME_LearnAcrobat = "Акробатика";
+const string NAME_LearnSneak = "Подкрадывание";
+const string NAME_RaiseStrength = "Сила +";
+const string NAME_RaiseDexterity = "Ловкость +";
+const string NAME_RaiseStrDex = "Сила и ловкость +";
+const string NAME_RaiseManaMax = "Максимальная мана +";
+const string NAME_RaiseHealthMax = "Максимальная жизнь +";
+const string NAME_CorristoHighRobe = "Господин мой, я хочу носить облачение высших магов Огня.";
+const string NAME_SaturasHighRobe = "Облачение высших магов";
+const string NAME_DiegoHeavyShadows = "Тяжелый доспех призраков. Защита: от оружия 40, от стрел 5, от огня 20";
+const string NAME_StoneHeavyGuards = "Тяжелый доспех стражника. Защита: от оружия 70, от стрел 10, от огня 35";
+const string NAME_StoneGuards = "Доспех стражника. Защита: от оружия 55, от стрел 10, от огня 25";
+const string NAME_WolfBandits = "Доспех вора. Защита: от оружия 35, от стрел 5, от огня 15";
+const string NAME_WolfHeavyBandits = "Тяжелый доспех вора. Защита: от оружия 40, от стрел 5, от огня 20";
+const string NAME_LeeMercs = "Доспех наемника. Защита: от оружия 55, от стрел 10, от огня 25";
+const string NAME_LeeHeavyMercs = "Тяжелый доспех наемника. Защита: от оружия 70, от стрел 10, от огня 35";
+const string NAME_FiskLightDiggers = "Простые штаны рудокопа. Защита: от оружия 10, от огня 5";
+const string NAME_FiskDiggers = "Штаны рудокопа. Защита: от оружия 15, от огня 5";
+const string NAME_GorNaTothHeavyTpl = "Тяжелый доспех стража. Защита: от оружия 70, от стрел 10, от огня 35";
+const string NAME_GorNaTothTpl = "Доспех стража. Защита: от оружия 55, от стрел 10, от огня 25";
+const string NAME_TorwachenPsiRock = "Набедренная повязка. Защита: от оружия 15, от огня 10";
+const string MENU_TEXT_NEEDS_APPLY = "";
+const string MENU_TEXT_NEEDS_RESTART = "Некоторые настройки вступят в силу только после перезагрузки.";
+const string STR_INFO_TRADE_ACCEPT = "Принять";
+const string STR_INFO_TRADE_RESET = "Отказаться";
+const string STR_INFO_TRADE_EXIT = "Назад";
