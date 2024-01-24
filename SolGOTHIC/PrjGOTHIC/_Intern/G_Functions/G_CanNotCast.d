9c9d8eb81f44b1ee40ebd0764d61d2e178057f11
z0n4r1e <135639955+z0n4r1e@users.noreply.github.com>
2024-01-24 18:38:05 +0200

    Add files via upload

--- SolGOTHIC/PrjGOTHIC/_Intern/G_Functions/G_CanNotCast.d
+++ SolGOTHIC/PrjGOTHIC/_Intern/G_Functions/G_CanNotCast.d
@@ -1,1 +1,30 @@
 
+func void G_CanNotCast(var int bIsPlayer,var int nCircleNeeded,var int nCirclePossessed)
+{
+	var int nDifference;
+	var string strDifference;
+	var string strMessage;
+	nDifference = nCircleNeeded - nCirclePossessed;
+	strDifference = IntToString(nDifference);
+	if(bIsPlayer)
+	{
+		strMessage = _STR_CANNOTUSE_PRE_PLAYER;
+	}
+	else
+	{
+		return;
+	};
+	strMessage = ConcatStrings(self.name,_STR_CANNOTUSE_PRE_NPC);
+	strMessage = ConcatStrings(strMessage,IntToString(self.id));
+	strMessage = ConcatStrings(strMessage,_STR_CANNOTUSE_POST_NPC);
+	strMessage = ConcatStrings(strMessage,strDifference);
+	strMessage = ConcatStrings(strMessage," ");
+	strMessage = ConcatStrings(strMessage,_STR_CANNOTUSE_LEVELS);
+	strMessage = ConcatStrings(strMessage,_STR_ATTRIBUTE_MAGIC_CIRCLE);
+	strMessage = ConcatStrings(strMessage,_STR_CANNOTUSE_POST);
+	if(bIsPlayer)
+	{
+		G_PrintScreen(strMessage);
+	};
+};
+
